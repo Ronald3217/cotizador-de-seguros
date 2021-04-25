dfe31a5ced7c4e40a5ba5d33a522a6f3cd87206f
ronald327 <ronald3217@gmail.com>
2021-04-24 20:59:18 -0600

    Update Resumen.js

--- src/componentes/Resumen.js
+++ src/componentes/Resumen.js
@@ -15,7 +15,7 @@ const Resumen = ({datos}) => {
     if ( marca === '') return null
     return ( 
         <ContenedorResumen>
-            <h2>Resumen de cotizacion</h2>
+            <h2>Resumen de Cotización</h2>
             <ul>
                 <li>Marca: {primerMayuscula(marca)}</li>
                 <li>Plan: {primerMayuscula(plan)}</li>
